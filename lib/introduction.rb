74b95c7c462990f2d17d1b7daa29bc00846c4df8
Gregory Thomas <gregorythomasjr55@gmail.com>
2019-06-25 13:32:28 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,9 +1,9 @@
-def introduction(name)
-  puts "Hi, my name is #{name}."
+def introduction(Greg)
+  puts "Hi, my name is #{Greg}."
 end
 
-def introduction_with_language(name, language)
-  puts "Hi, my name is #{name} and I am learning to program in #{language}."
+def introduction_with_language(Greg, English)
+  puts "Hi, my name is #{name} and I am learning to program in #{English}."
 end
 
 def introduction_with_language_optional(name, language="Ruby")
